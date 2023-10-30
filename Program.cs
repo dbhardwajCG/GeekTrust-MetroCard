ea49b077bc93058f577f538fe802e7b7c43e19f7
divakar bhardwaj <dbhardwaj@contecglobal.com>
2023-10-30 18:13:37 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GeekTrust
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] inputData = File.ReadAllLines(args[0]);
+                List<string> inputDataAsString = inputData.ToList<string>();
+                //Add your code here to process the input commands
+                inputDataAsString.ForEach(input => Console.WriteLine(input));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
